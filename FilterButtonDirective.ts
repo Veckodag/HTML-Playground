1a0453f10b69ef84dc3fd6ec8dd9eac7832c4e15
Fredrik Sandström <Fredrik.Sandstrom@triona.se>
2017-06-02 10:54:52 +0200

    Initial commit for a relevant files

---
+++ FilterButtonDirective.ts
@@ -0,0 +1,15 @@
+﻿module app.directives {
+
+  "use strict";
+
+  import SearchCtrl = app.SearchCtrl;
+
+  angular.module("app.directives").directive("searchFilterButtons", () => {
+    return {
+      restrict: "E",
+      templateUrl: "/SPA/views/filterButtons.html",
+      controller: SearchCtrl,
+      controllerAs: "SearchCtrl"
+    }
+  });
+}
